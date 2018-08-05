d4314c4efbe0014b25251914bc331d236d5542bb
sri krishna chekuri <sri.chekuri@maryland.gov>
2018-08-05 00:20:24 -0400

    test push

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -3,11 +3,10 @@ import { CommonModule, } from '@angular/common';
 import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import {UserHomeComponent} from "./user-home/user-home.component";
-import {ProfileComponent} from "./profile/profile.component";
 
 
 const routes: Routes =[
-    {path:'home', component: ProfileComponent},
+    {path:'home', component: UserHomeComponent},
 ];
 
 export const UserRouting = RouterModule.forChild(routes);
